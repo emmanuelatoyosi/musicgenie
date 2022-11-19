ce6e682453cd11631a00aa425f1d21b18f040ba5
Emmanuela <omotoyosiemma@gmail.com>
2022-11-19 13:03:51 -0500

    intial commit

---
+++ musicgenie/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from music_app.views import LandingPageView,ArtistCreateView,ArtistListView,ArtistUpdateView,deleteArtist
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',LandingPageView.as_view(),name='home'),
+    path('artists',ArtistListView.as_view(),name='artists'),
+path('add_artist',ArtistCreateView.as_view(),name='add_artist'),
+path('artist-details/<int:pk>', ArtistUpdateView.as_view(),name='artist_details'),
+path('artist-delete/<int:pk>', deleteArtist,name='delete_artist')
+]
